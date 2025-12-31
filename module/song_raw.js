086953bcd1b57cddace4fd74a6db31289b752768
oooonly <26619678+oooonly@users.noreply.github.com>
2026-01-01 00:05:18 +0800

    Update song_raw.js

--- module/song_raw.js
+++ module/song_raw.js
@@ -10,7 +10,9 @@ module.exports = (params, useAxios) => {
   }).then(response => {  
     // 设置响应头，不是请求头  
     response.headers = {  
-      'Content-Type': 'audio/mpeg'  
+      'Content-Type': 'audio/mpeg',
+      'Access-Control-Allow-Origin': '*',  
+      'Access-Control-Allow-Methods': '*',
     };  
     return response;  
   });  
