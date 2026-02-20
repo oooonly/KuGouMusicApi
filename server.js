b003f317abbfdbbf1457a8073e4a6667c8c1bb2e
oooonly <oooohoya@qq.com>
2026-02-21 00:19:40 +0800

    Update server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ async function getModulesDefinitions(modulesPath, specificRoute, doRequire = tru
  * @param {ModuleDefinition[]} moduleDefs
  * @return {Promise<import('express').Express>}
  */
-async function consturctServer(moduleDefs) {
+export async function consturctServer(moduleDefs) {
   const app = express();
   const { CORS_ALLOW_ORIGIN } = process.env;
   app.set('trust proxy', true);
